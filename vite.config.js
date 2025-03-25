6b4f3445bfd0e04bf88b84ad2297638f10b0baf0
Shashank <shashankbmusale07@gmail.com>
2025-03-25 15:12:30 +0530

    add some changes

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  server: {
+    port: 1234, // or any other port
+  },
+  build: {
+    outDir: "dist",
+  },
 });
